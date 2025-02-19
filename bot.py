6aa54222c599309a68901d7a9134b4447cfde273
jekakiev <60492248+jekakiev@users.noreply.github.com>
2025-02-19 17:32:51 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -131,7 +131,7 @@ async def edit_wallet_command(message: types.Message):
 # Функция для регистрации обработчиков команд и сообщений
 def register_handlers(dp: Dispatcher):
     dp.message.register(start_command, Command("start"))
-    dp.message.register(edit_wallet_command, Command("EDITw"))
+    dp.message.register(edit_wallet_command, 
 
 # Запуск бота
 async def main():
