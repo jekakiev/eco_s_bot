9021f53543bf403b5dbe4f6f4671905e772d6d79
jekakiev <60492248+jekakiev@users.noreply.github.com>
2025-02-22 12:21:33 +0200

    2

--- interface/__init__.py
+++ interface/__init__.py
@@ -1,4 +1,4 @@
 from .states import WalletStates
 from .keyboards import get_main_menu, get_back_button, get_wallets_list, get_wallet_control_keyboard, get_tokens_keyboard
-from .callbacks import show_wallets, add_wallet_start, process_wallet_address, process_wallet_name, toggle_token, confirm_tokens, delete_wallet, rename_wallet_start, process_new_wallet_name, edit_wallet, go_home
+from .callbacks import show_wallets, add_wallet_start, process_wallet_address, process_wallet_name, toggle_token, confirm_tokens, delete_wallet, rename_wallet_start, process_new_wallet_name, go_home
 from .handlers import register_handlers
