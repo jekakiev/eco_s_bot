d49d3d956b95fc18d9ba0f8a237f2ba00b1f57e5
jekakiev <60492248+jekakiev@users.noreply.github.com>
2025-03-01 04:22:12 +0200

    Update handlers.py

--- interface/handlers.py
+++ interface/handlers.py
@@ -1,6 +1,6 @@
 # /interface/handlers.py
 from aiogram import Dispatcher, F, types
-from aiogram.filters import Command, Text  # Добавляем Text
+from aiogram.filters import Command
 from .callbacks.wallets import (
     show_wallets, add_wallet_start, process_wallet_address, process_wallet_name,
     toggle_token, confirm_tokens, save_tokens, delete_wallet, rename_wallet_start,
@@ -25,6 +25,7 @@ from .callbacks.test_api_by_hash import (
 from .states import WalletStates, TokenStates, SettingStates
 from db import Database
 from utils.logger_config import logger, should_log
+import re  # Добавляем для проверки регулярного выражения
 
 db = Database()
 
@@ -82,8 +83,8 @@ def register_handlers(dp: Dispatcher):
         if should_log("interface"):
             logger.warning("Нет кошельков для регистрации команд /Editw_XXXX")
     
-    # Регистрируем edit_token_command с фильтром Text для всех /edit_XXXX
-    dp.message.register(edit_token_command, Text(startswith="/edit_", regex=r"^/edit_\w{4}$"))
+    # Регистрируем edit_token_command с фильтром F.text и проверкой через regex
+    dp.message.register(edit_token_command, F.text.regexp(r"^/edit_\w{4}$"))
 
     dp.message.register(get_thread_id_command, Command(commands=["get_thread_id"]))
 
